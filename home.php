081d1a06b471339bdc04b2dc5bef5ec9141b14bc
Sayeeram09 <109091491+Sayeeram09@users.noreply.github.com>
2023-03-11 10:59:37 +0530

    Add files via upload

---
+++ home.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link rel="stylesheet" href="css/style.css" />
+    <title>Student Attendance</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/login.js"></script>
+    <!-- Custom styles for this template -->
+    <link href="navbar-fixed-top.css" rel="stylesheet">
+</head>
+
+<body style="background-color:blue">
+    <!-- Fixed navbar -->
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar"
+                    aria-expanded="false" aria-controls="navbar">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.php">Attendance Monitoring</a>
+            </div>
+            <div id="navbar" class="navbar-collapse collapse">
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="active"><a href="#">Home</a></li>
+                    <li><a href="home.php">About</a></li>
+                    <li><a href="home.php">Contact</a></li>
+
+                </ul>
+            </div><!--/.nav-collapse -->
+        </div>
+    </nav>
+    <section style="margin-top:100px;">
+        <div class="text-center">
+            <h1 style="color:white;">M. Sayee ram</h1>
+            <h2 style="color:white;">3rd year B.Sc Computer Science</h2>
+            <h3 style="color:white;">Bishop Heber College</h3>
+        </div>
+    </section>
+</body>
+
+</html>
